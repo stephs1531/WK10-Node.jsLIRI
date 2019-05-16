7b6fa622e7c73b6d2f5ef5c9df9f8d622f31a9f0
Stephanie Sapp <stephs1531@gmail.com>
2019-05-16 08:15:44 -0500

    added necessary "require" lines, install packages

--- liri.js
+++ liri.js
@@ -1,1 +1,20 @@
 //LIRI command line interface
+require("dotenv").config();
+
+//require stuff to make spotify work
+var Spotify = require("node-spotify-api");
+var keys = require("./keys.js");
+var newSpotify = new Spotify(keys.spotify);
+
+var request = require("request");
+var inquirer = require("inquirer");
+// var colors = require("colors");
+var fs = require("fs");
+
+//call the function to start LIRI
+startLiri();
+
+//LIRI function
+function startLiri() {
+    console.log("stuff happens!");
+}
