334983b53d16d6f21f30f20af221eb8da9dce05c
Stephanie Sapp <stephs1531@gmail.com>
2019-05-16 09:20:49 -0500

    spotify working...ish?

--- liri.js
+++ liri.js
@@ -16,7 +16,7 @@ startLiri();
 function startLiri() {
     console.log("stuff happens!");
 
-    //prompt the user to pick which option they want to run
+    // prompt the user to pick which option they want to run
     inquirer.prompt([
         {
             type: "list",
@@ -48,6 +48,8 @@ function startLiri() {
                     ]) .then ((answers) => {
                         var song = answers.song;
                         musicInfo(song);
+                        //as of 5/16 AM...this is *sort of* working but it's returning weird shit for all the variables
+
                         // console.log(song);
                     })
                 break;
@@ -64,6 +66,7 @@ function startLiri() {
                 console.log("Please make a selection");
             }
     })
+
 }
 
 //spotify function
@@ -71,26 +74,34 @@ function musicInfo(song) {
 
     //get api keys for spotify
     var newSpotify = new Spotify(keys.spotify);
+    console.log(newSpotify);
 
     newSpotify.search({
         type: "track",
         query: "song",
         limit: 1
-    }), (err, data) => {
+    }).then((data) => {
         console.log(data);
-        if (err) {
-            console.log(`\n${err}\n`);
-        }
+        console.log(data.items);
+        // if (err) {
+        //     console.log(`\n${err}\n`);
+        // }
 
         //create variables for spotify information we want
         let artist = data.tracks.items[0].album.artists[0].name;
 		let title = data.tracks.items[0].name;
 		let URL = data.tracks.items[0].album.artists[0].external_urls.spotify;
         let album = data.tracks.items[0].album.name;
+
+        console.log(artist);
+        console.log(title);
+        console.log(URL);
+        console.log(album);
+
         
         console.log("\nYour Song: \n");
         console.log("${title} by ${artist} on the album ${album}");
-        console.log("Listen at ${url}");
-    }
+        console.log("Listen at ${URL}");
+    });
 
 }
